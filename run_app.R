cb0ec5b3886baa7e5683995851c6737c872d4f8f
Francesco Patane, MSc <francesco.patane@live.it>
2026-04-23 21:46:02 +0200

    Update run_app.R

--- run_app.R
+++ run_app.R
@@ -1,3 +1,3 @@
 #!/usr/bin/env Rscript
 library(scTAMsExplorer)
-tuopacchetto::launch_app(host = "0.0.0.0", port = 3838, launch.browser = FALSE)
+scTAMsExplorer::launch_app(host = "0.0.0.0", port = 3838, launch.browser = FALSE)
