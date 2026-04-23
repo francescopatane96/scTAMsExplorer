f66b14a42c1b358a2824ecb8ddf6adacc0b56009
Francesco Patane, MSc <francesco.patane@live.it>
2026-04-23 20:02:41 +0200

    Update css.R

--- css.R
+++ css.R
@@ -5,7 +5,7 @@
 #' Read the app CSS from the installed package file
 #' @keywords internal
 app_css <- function() {
-  css_file <- system.file("app/www/styles.css", package = "SeuratExplorer")
+  css_file <- system.file("styles.css", package = "SeuratExplorer")
   if (!nzchar(css_file)) {
     css_file <- file.path(
       system.file(package = "SeuratExplorer"), "styles.css"
